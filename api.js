eac0572055c786bb200663c0e5b405a888d8ef35
Traci <traci.face@gmail.com>
2021-04-10 09:29:39 -0400

    merging code

--- api.js
+++ api.js
@@ -1,100 +1,47 @@
-var tableBody = document.getElementById('');
-var fetchButton = document.getElementById('fetch-button');
+var imgDiv = document.getElementById('container background-img')
+var searchBtn = document.getElementById('plantSearch');
 const apiKey = 'token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc';
 var plant;
-​
-​
-​
-//   // fetch request gets a list of all the repos for the node.js organization
-//   var requestUrl = 'https://trefle.io/api/v1/plants?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc';
-​
-// https://trefle.io/api/v1/species/quercus-rotundifolia?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc
-//     return response.json(); // parses JSON response into native JavaScript objects
-//   }
-// async function postData(url = requestUrl, data = {}) {
-//   // Default options are marked with *
-//   const response =fetch(url, {
-//     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-//     mode: 'no-cors', // no-cors, *cors, same-origin
-//     cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: 'include', // , *same-origin, omit
-//     headers: {
-//       'Content-Type': 'application/json'
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: 'follow', // manual, *follow, error
-//     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//     body: JSON.stringify(data) // body data type must match "Content-Type" header
-//   });
-​
-// postData()
-//   .then(data => {
-//     console.log(data); // JSON data parsed by `data.json()` call
-// //   });
-// fetch('https://trefle.io/api/v1/plants?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc')
-// .then(function (response) {
-//   return response.json();
-// }).then(function(data) {
-//   console.log(data)
-// https://trefle.io/api/v1/plants/search?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc&q=common_strawberry
-// .catch(function (err) {
-//   console.log(err)
-// })
-//+ plant.data[0].scientific_name
-$(function () {
-  // $.ajax({
-  //   url: 'https://trefle.io/api/v1/species/quercus-rotundifolia?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc',
-  //   method: 'GET',
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*'
-  //   }
-  // }).then(function (response) {
-  //   species = response
-  //   console.log(species.specifications)
-  // })
-​
-  function getApi () {
+  $("#plantSearch" ).click(function() {
+    var searchTerm = $("#plantInput").val()
+    getPlantInfo(searchTerm)
+    $(".right-side").html('')
+  });
+function  getPlantInfo (searchTerm) {
     $.ajax({
-      url: 'https://trefle.io/api/v1/plants/search?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc&q=okra',
+       url: ' https://trefle.io/api/v1/species?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc&filter[common_name]=' + searchTerm,
+     // url: 'https://trefle.io/api/v1/plants/search?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc&q=' + searchTerm,
       method: 'GET',
       headers: {
         'Access-Control-Allow-Origin': '*'
       }
     }).then(function (responsePl) {
-      // console.log(responsePl.data[0].common_name)
+      console.log(responsePl.data)
       // console.log(responsePl.data[0].id)
       var plantId = document.createElement('p')
       var plantDiv = document.createElement("div")
       var plantH1 = document.createElement("h1")
       var plantP = document.createElement("p")
-      var plantImg = document.createElement('img')
+      //   var plantImg = document.createElement('img')
       var plantFam = document.createElement('p')
-      
-​
       plant = responsePl
       // console.log(plant)
       // console.log(plant.links.self)
-​
       plantId.textContent = responsePl.data[0].id
       plantH1.textContent = responsePl.data[0].common_name
       plantP.textContent = responsePl.data[0].scientific_name
       plantFam.textContent = responsePl.data[0].family
       var plantSn = (responsePl.data[0].scientific_name).split(" ")
-    
-      // console.log(plantSn)
-      plantImg.src = responsePl.data[0].image_url
-      // var newUrl = 'https://trefle.io/api/v1/species/' + plantP.textContent + '?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc'
-​
-      // plantGrow.textContent = 'https://trefle.io/api/v1/species/' + plantSn.textContent + '?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc'
-      // common name
-      plantDiv.append(plantH1)
-      // scienctific name
-      plantDiv.append(plantP)
+      // console.log(plant.data.distribution.native)
+      plantImg = responsePl.data[0].image_url
       // plant img
-      plantDiv.append(plantImg)
+      $(".mainplantimg").attr("src", plantImg);
+      plantDiv.append(plantH1)
+      // scienctific name     
+      $(".right-side").append(plantH1)
+      $(".right-side").append(plantP)
       // family 
-      plantDiv.append(plantFam)
-      $("#repo-table").append(plantDiv)
+      $(".right-side").append(plantFam)
       //console.log(plantSn)
       $.ajax({
         url: 'https://trefle.io/api/v1/species/' + plantSn[0] + "-" + plantSn[1] + '?token=tGcyeSLgtYKukjgY8iqyFaUrpO9lWKqDxP9QtYcrfMc',
@@ -112,12 +59,10 @@ $(function () {
           plantNative = responseSp.data.distribution.native[i]
           plantNative.replace("", " ")
           // native zones 
-          plantDiv.append(plantNative + " ")
+          $(".right-side").append(plantNative + ", ")
           // console.log(native)
         }
       })
       //Loop over the data to generate a table, each table row will have a link to the repo url
     })
-  }
-  fetchButton.addEventListener('click', getApi);
-})
+}
--- zone.js
+++ zone.js
@@ -1,6 +1,6 @@
 let zipCodeTB = document.getElementById('zipCodeTB')
-let submitButton = document.getElementById('submitButton')
-let resultsContainer = document.getElementById('resultsContainer')
+let submitZoneButton = document.getElementById('zipSearch')
+let resultsZoneContainer = document.getElementById('resultsZoneContainer')
 
 async function fetchZone(zip) {
     let response = await fetch(`https://phzmapi.org/${zip}.json`)
@@ -8,7 +8,7 @@ async function fetchZone(zip) {
     return zoneResults
 }
 
-submitButton.addEventListener('click', async () => {
+submitZoneButton.addEventListener('click', async () => {
 
     let zipCode = zipCodeTB.value
 
@@ -18,19 +18,74 @@ submitButton.addEventListener('click', async () => {
 
         let result = await fetchZone(zipCode)
 
-        let zone = `<div class="zoneDisplay">
-            <h2> Your Growing Zone </h2>
-            <span>Zone: ${result.zone}</span>
-            <span>Temp Range: ${result.temperature_range}</span>
-            <span>Lat: ${result.coordinates.lat}</span>
-            <span>Lon: ${result.coordinates.lon}</span>
-            </div>`
+        var zipHeader = document.createElement("h4")
+        var zipZone = document.createElement("p")
+        var zipTemp = document.createElement("p")
+        var zipLon= document.createElement("p")
+        var zipLat = document.createElement("p")
 
-        resultsContainer.innerHTML = zone
-        zipCodeTB.value = ''
+        zipHeader.textContent = "Your Growing Zone"
+        zipZone.textContent = result.zone
+        zipTemp.textContent = result.temperature_range
+        zipLon.textContent = result.coordinates.lon
+        zipLat.textContent = result.coordinates.lat
+
+        $(".container").append(zipHeader)
+        $(".container").append(zipZone)
+        $(".container").append(zipTemp)
+        $(".container").append(zipLon)
+        $(".container").append(zipLat)
+
+        // let zone = `<div class="zoneDisplay">
+        //     <h2> Your Growing Zone </h2>
+        //     <span>Zone: ${result.zone}</span>
+        //     <span>Temp Range: ${result.temperature_range}</span>
+        //     <span>Lat: ${result.coordinates.lat}</span>
+        //     <span>Lon: ${result.coordinates.lon}</span>
+        //     </div>`
+
+        // resultsZoneContainer.innerHTML = zone
+        // zipCodeTB.value = ''
 
     } else {
-        resultsContainer.innerHTML = 'Please enter a valid zip code.'
+        resultsZoneContainer.innerHTML = 'Please enter a valid zip code.'
         zipCodeTB.value = ''
     }
 })
+
+// let plantName = document.getElementById('plantName')
+// let submitPlantButton = document.getElementById('submitPlantButton')
+// let resultsPlantContainer = document.getElementById('resultsPlantContainer')
+
+// async function fetchPlant(zip) {
+//     let response = await fetch(`https://phzmapi.org/${zip}.json`)
+//     let zoneResults = await response.json()
+//     return zoneResults
+// }
+
+// submitZoneButton.addEventListener('click', async () => {
+
+//     // let zipCode = zipCodeTB.value
+
+//     // // const zipCodeRegex = /^\d{5}$/
+
+//     // // if (zipCodeRegex.test(zipCode)) {
+
+//     // //     let result = await fetchZone(zipCode)
+
+//         let zone = `<div class="zoneDisplay">
+//             <h2> Your Growing Zone </h2>
+//             <span>Zone: ${result.zone}</span>
+//             <span>Temp Range: ${result.temperature_range}</span>
+//             <span>Lat: ${result.coordinates.lat}</span>
+//             <span>Lon: ${result.coordinates.lon}</span>
+//             </div>`
+
+//         resultsPlantContainer.innerHTML = zone
+//         zipCodeTB.value = ''
+
+//     // } else {
+//     //     resultsZoneContainer.innerHTML = 'Please enter a valid zip code.'
+//     //     zipCodeTB.value = ''
+//     // }
+// })
